ccac7b24d2f23419ebef93cbe17781035680e52f
mprussiani <mprussiani@localhost>
2018-02-21 15:37:37 +0100

    upd

--- src/main/java/com/sorint/openshift/ServiceApi.java
+++ src/main/java/com/sorint/openshift/ServiceApi.java
@@ -7,14 +7,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @RequestScoped
-@Path("/insult")
+@Path("/generate")
 public class ServiceApi {
 
 	@GET()
 	@Produces("application/json")
 	public HashMap<String, String> getInsult() {
 		HashMap<String, String> theInsult = new HashMap<String, String>();
-		theInsult.put("insult", new ServiceDb().generate());
+		theInsult.put("generate: ", new ServiceDb().generate());
 		return theInsult;
 	}
 }
